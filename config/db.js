75f03f53fe1ffd15009182ac00e3f35cc31c78ee
sid-siddartha <psiddarthagoud9@gmail.com>
2026-07-21 22:39:43 +0530

    database changes

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 const mongoose = require('mongoose');
 const User = require('../models/User');
 const MenuItem = require('../models/MenuItem');
